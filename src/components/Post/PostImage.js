18cb9f9a47487880d105aa8e3bdda50891f97f7a
Janos Kocs <janos.kocs@outlook.com>
2022-10-15 15:03:44 +0100

    Slider fix

--- src/components/Post/PostImage.js
+++ src/components/Post/PostImage.js
@@ -3,17 +3,12 @@ import "slick-carousel/slick/slick-theme.css"
 import Slider from "react-slick"
 const PostImage = ({ media }) => {
 
-    const imageObjectsArray = media.map((image) => {
-        return { original: image.url }
-    })
-
     const settings = {
         dots: false,
-        infinite: false,
-        speed: 1000
+        infinite: false
     }
     return (
-        <Slider className="w-6/12"{...settings}>
+        <Slider className="w-6/12" {...settings}>
             {media.map((image) => (
                 <img key={image.blur_hash} src={image.url} alt="" />
             ))}
