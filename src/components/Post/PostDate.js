e05a9e8980cbd3e2c9deff6626f3263149d4dbb5
Janos Kocs <janos.kocs@outlook.com>
2022-10-15 18:46:24 +0100

    Install and implement time ago functionality

--- src/components/Post/PostDate.js
+++ src/components/Post/PostDate.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import TimeAgo from 'javascript-time-ago'
+import en from 'javascript-time-ago/locale/en.json'
+import ReactTimeAgo from 'react-time-ago'
 
 const PostDate = ({ postDate }) => {
-    return <p>{postDate}</p>
+    TimeAgo.addDefaultLocale(en)
+
+    return <p><ReactTimeAgo date={postDate} locale="en-US" /></p>
 }
 
 export default PostDate
