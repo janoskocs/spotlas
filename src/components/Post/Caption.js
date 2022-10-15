3b74730db6b830d80095be32687d4c04872c27bb
Janos Kocs <janos.kocs@outlook.com>
2022-10-15 23:23:20 +0100

    Truncate Text with more and less buttons

--- src/components/Post/Caption.js
+++ src/components/Post/Caption.js
@@ -1,14 +1,44 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
 import UserName from './User/UserName'
-
+import truncate from 'lodash/truncate'
 
 const Caption = ({ username, caption }) => {
-    return (
-        <div className="caption">
-            <UserName className="inline" userName={username} />
-            <p className="inline truncate ...">{caption.text}</p>
-        </div>
-    )
+
+    const [fullText, setCaptionText] = useState(caption.text)
+    const [truncatedText, setTruncatedText] = useState('')
+    const [truncated, setTruncated] = useState(true)
+    const [currentText, setCurrentText] = useState('')
+
+    useEffect(() => {
+        setTruncatedText(truncate(fullText, { length: 55, omission: ' ' }))
+        setCurrentText(truncatedText)
+    }, [])
+
+    const handleTruncate = (boolean) => {
+        if (boolean) {
+            setTruncated(false)
+            setCurrentText(fullText)
+        } else {
+            setTruncated(true)
+            setCurrentText(truncatedText)
+        }
+    }
+    if (fullText.length < 55) {
+        return (
+            <div className="caption">
+                <UserName className="inline" userName={username} />
+                <p className="inline">{fullText}</p>
+            </div>
+        )
+    } else {
+        return (
+            <div className="caption">
+                <UserName className="inline" userName={username} />
+                <p className="inline">{currentText}</p>
+                {truncated ? <button onClick={() => handleTruncate(true)}>more...</button> : <button onClick={() => handleTruncate(false)}> Less</button>}
+            </div >
+        )
+    }
 }
 
 export default Caption
