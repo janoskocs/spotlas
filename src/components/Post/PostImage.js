af3f7aef2d27f1d3c0980f8bff8bb7359bdaea6f
Janos Kocs <janos.kocs@outlook.com>
2022-10-16 10:40:08 +0100

    Fix output and Heart import

--- src/components/Post/PostImage.js
+++ src/components/Post/PostImage.js
@@ -1,7 +1,6 @@
 import "slick-carousel/slick/slick.css"
 import "slick-carousel/slick/slick-theme.css"
 import Slider from "react-slick"
-import User from './User/User'
 import Heart from "./Heart"
 const PostImage = ({ showHeart, handleLike, userPostId, media }) => {
 
