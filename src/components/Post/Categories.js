a01049de8ae21616c34a736f23ad28bc1903fd45
Janos Kocs <janos.kocs@outlook.com>
2022-10-17 21:01:13 +0100

    Add categories

--- src/components/Post/Categories.js
+++ src/components/Post/Categories.js
@@ -1,8 +1,16 @@
 import React from 'react'
 
 const Categories = () => {
+
+    const categories = ['Outdoors', 'Cheap', 'Live Music', 'Fancy', 'Romantic', 'Fun', 'Exotic', 'Extraordinary', 'Sweet', 'Innovative', 'Interesting']
     return (
-        <div>Categories</div>
+        <div className="categories flex  whitespace-nowrap items-start mt-1 md:mt-2 l:mt-3 ">
+            {categories.map((category, index) => (
+                <div key={index} className="category  inline-block drop-shadow  p-2 border-2 rounded-md ml-0.5 md:ml-1 l:ml-1.5">
+                    <p>{category}</p>
+                </div>
+            ))}
+        </div>
     )
 }
 
