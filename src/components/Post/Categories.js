da015a776eee7f0e9d4e625c1b67a060e7838e4a
Janos Kocs <janos.kocs@outlook.com>
2022-10-17 21:54:52 +0100

    Add categories scroll

--- src/components/Post/Categories.js
+++ src/components/Post/Categories.js
@@ -1,16 +1,25 @@
-import React from 'react'
-
+import { useRef } from 'react';
+import useScrollOnDrag from 'react-scroll-ondrag';
 const Categories = () => {
 
     const categories = ['Outdoors', 'Cheap', 'Live Music', 'Fancy', 'Romantic', 'Fun', 'Exotic', 'Extraordinary', 'Sweet', 'Innovative', 'Interesting']
+
+    const ref = useRef();
+    const { events } = useScrollOnDrag(ref);
+
     return (
-        <div className="categories flex  whitespace-nowrap items-start mt-1 md:mt-2 l:mt-3 ">
+        <div
+            {...events}
+            ref={ref}
+            className="categories flex  whitespace-nowrap overflow-x-scroll items-start mt-1 md:mt-2 l:mt-3 "
+        >
             {categories.map((category, index) => (
-                <div key={index} className="category  inline-block drop-shadow  p-2 border-2 rounded-md ml-0.5 md:ml-1 l:ml-1.5">
+                <div key={index} className="category  inline-block drop-shadow  p-2 border-2 rounded-md ml-0.5 md:ml-1 l:ml-1.5 select-none" >
                     <p>{category}</p>
                 </div>
-            ))}
-        </div>
+            ))
+            }
+        </div >
     )
 }
 
